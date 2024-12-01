1be8bd891d69cc6e202db00f23ce21c952d63db6
vmkiss <veronika.kiss17@gmail.com>
2024-12-01 15:40:47 -0800

    implement and test function to generate random playlist

--- server.py
+++ server.py
@@ -2,16 +2,25 @@ import zmq
 import time
 import random
 
-def get_indices(length):
-    indices = []
-    for _ in range(length):
-        pass
-    return indices
-
 # Establish socket connection
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://localhost:5555")
+
+def create_playlist(indices, data):
+    playlist = ''
+    for i in range(len(indices)):
+        song = data[indices[i]]
+        song_data = song.split('*')
+        title = song_data[0]
+        artist = song_data[1]
+        album = song_data[2]
+
+        song_string = ''
+        song_string += "SONG #" + str(i+1) + "\n" + "Title: " + title + "\n" + "Artist: " + artist + "\n" + "Album: " + album + "\n"
+        playlist += song_string + "\n"
+
+    return playlist
 
 while True:
     message = socket.recv_string()
@@ -25,9 +34,11 @@ while True:
             break
         else:
             data = message.split('\n')
-            length = data[0]
+            playlist_len = int(data[0])
             data = data[1:]
+            data_len = len(data)
+            print(data)
+            indices = random.sample(range(data_len), playlist_len)
+            playlist = create_playlist(indices, data)
 
-            rand_indices = get_indices(length)
-
-        socket.send_string(result)
+        socket.send_string(playlist)
